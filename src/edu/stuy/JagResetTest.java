093aa915d5b8c3304a5533e76b0227d2410de767
Prog694 <prog694@gmail.com>
2012-03-05 18:56:56 -0500

    Automatically resets Jag when timed out. Works! (sometimes)

--- src/edu/stuy/JagResetTest.java
+++ src/edu/stuy/JagResetTest.java
@@ -4,9 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.stuy;
-
 
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.can.CANTimeoutException;
@@ -21,38 +19,49 @@ import edu.wpi.first.wpilibj.networktables.NetworkTableKeyNotDefined;
  * directory.
  */
 public class JagResetTest extends SimpleRobot {
+
     CANJaguar jag;
     CurrentThing currentThing;
     Joystick jstick;
-
+    boolean isJaguarTimedOut;
 
     public JagResetTest() {
+        isJaguarTimedOut = false;
         currentThing = new CurrentThing(4);
         currentThing.start();
         jstick = new Joystick(1);
+        try {
+            configureJag();
+        }
+        catch (CANTimeoutException e) {
+            isJaguarTimedOut = true;
+        }
     }
 
     public void robotInit() {
         try {
-            resetJag();
-        }
-        catch (Exception e) {
-            e.printStackTrace();
+            resetJagIfBroken();
+        } catch (Exception e) {
         }
     }
 
-    public void resetJag() {
-        try {
+    public final void configureJag() throws CANTimeoutException {
         jag = new CANJaguar(3);
         jag.setPID(1, 0, 0);
         jag.changeControlMode(CANJaguar.ControlMode.kSpeed);
         jag.setSpeedReference(CANJaguar.SpeedReference.kEncoder);
         jag.configEncoderCodesPerRev(250);
         jag.enableControl();
-        }
-        catch (CANTimeoutException e) {
-          System.out.println("In resetJag: ");
-          e.printStackTrace();
+    }
+
+    public void resetJagIfBroken() {
+        try {
+            if (isJaguarTimedOut) {
+                configureJag();
+                isJaguarTimedOut = false;
+            }
+        } catch (CANTimeoutException e) {
+            isJaguarTimedOut = true;
         }
     }
 
@@ -60,7 +69,6 @@ public class JagResetTest extends SimpleRobot {
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
     }
 
     /**
@@ -68,25 +76,22 @@ public class JagResetTest extends SimpleRobot {
      */
     public void operatorControl() {
         while (isOperatorControl() && isEnabled()) {
-            if (jstick.getRawButton(1)) {
-                resetJag();
-            }
+            resetJagIfBroken();
             try {
                 jag.setPID(SmartDashboard.getDouble("P"), SmartDashboard.getDouble("I"), SmartDashboard.getDouble("D"));
                 jag.setX(SmartDashboard.getDouble("setpoint"));
-            }
-            catch (NetworkTableKeyNotDefined e) {
+            } catch (NetworkTableKeyNotDefined e) {
                 SmartDashboard.putDouble("P", 0);
                 SmartDashboard.putDouble("I", 0);
                 SmartDashboard.putDouble("D", 0);
                 SmartDashboard.putDouble("setpoint", 0);
-            }
-            catch (CANTimeoutException e) {
+            } catch (CANTimeoutException e) {
+                isJaguarTimedOut = true;
             }
 
             try {
                 SmartDashboard.putDouble("rpm", jag.getSpeed());
-                SmartDashboard.putInt("faults", (int)(jag.getFaults()));
+                SmartDashboard.putInt("faults", (int) (jag.getFaults()));
                 SmartDashboard.putDouble("bus voltage", jag.getBusVoltage());
                 SmartDashboard.putDouble("battery voltage", DriverStation.getInstance().getBatteryVoltage());
                 SmartDashboard.putDouble("output voltage", jag.getOutputVoltage());
@@ -94,7 +99,8 @@ public class JagResetTest extends SimpleRobot {
                 SmartDashboard.putDouble("tempreature", jag.getTemperature());
                 SmartDashboard.putBoolean("power cycled", jag.getPowerCycled());
 
-            } catch (Exception e) {
+            } catch (CANTimeoutException e) {
+                isJaguarTimedOut = true;
             }
 
             SmartDashboard.putDouble("Current thing", currentThing.getCurrent());
